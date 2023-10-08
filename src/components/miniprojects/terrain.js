e812d5423a3c5e0f94bb3222f5273e5d613f6ea9
lore64210 <lore64210@gmail.com>
2023-10-08 18:40:28 -0300

    added mini project test

---
+++ src/components/miniprojects/terrain.js
@@ -0,0 +1,69 @@
+"use client";
+import P5Container from "@/components/miniprojects/P5Container";
+import useWindowSize from "@/hooks/useWindowSize";
+import { useRef } from "react";
+
+const scale = 20;
+
+export default () => {
+    const windowSize = useWindowSize();
+    const flying = useRef(0);
+    const terrainRef = useRef();
+    const canvasSize = 1000;
+    const rows = canvasSize / scale;
+    const cols = rows;
+
+    const setup = (p5, canvasRef) => {
+        p5.createCanvas(600, 600, p5.WEBGL).parent(canvasRef);
+
+        const terrain = new Array(rows);
+        for (let i = 0; i < terrain.length; i++) {
+            terrain[i] = new Array(rows);
+        }
+        terrainRef.current = terrain;
+    };
+
+    const draw = (p5) => {
+        p5.background(0);
+        p5.stroke(255);
+
+        p5.noFill();
+        p5.rotateX(p5.PI / 2);
+        //p5.translate(windowSize.width / 5, -windowSize.height / 2, -500);
+        p5.translate(-windowSize.width * 1.5, -windowSize.height * 4, -300);
+        flying.current -= 0.05;
+        let yoff = flying.current;
+        for (let y = 0; y < cols; y++) {
+            let xoff = 0;
+            for (let x = 0; x < rows; x++) {
+                terrainRef.current[x][y] = p5.map(
+                    p5.noise(xoff, yoff),
+                    0,
+                    1,
+                    -250,
+                    100
+                );
+                xoff += 0.2;
+            }
+            yoff += 0.1;
+        }
+        for (let y = 0; y < cols - 1; y++) {
+            p5.beginShape(p5.TRIANGLE_STRIP);
+            for (let x = 0; x < rows; x++) {
+                p5.vertex(
+                    x * scale * 4,
+                    y * scale * 4,
+                    terrainRef.current[x][y]
+                );
+                p5.vertex(
+                    x * scale * 4,
+                    (y + 1) * scale * 4,
+                    terrainRef.current[x][y + 1]
+                );
+            }
+            p5.endShape();
+        }
+    };
+
+    return <P5Container draw={draw} setup={setup} overrideSetup />;
+};
