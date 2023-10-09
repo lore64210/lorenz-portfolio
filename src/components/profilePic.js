293a8047147aaa9808f0c766b230e00a8d485c8f
lore64210 <lore64210@gmail.com>
2023-10-09 10:11:15 -0300

    animation fix

--- src/components/profilePic.js
+++ src/components/profilePic.js
@@ -2,6 +2,7 @@
 import useIsMobile from "@/hooks/useIsMobile";
 import useWindowSize from "@/hooks/useWindowSize";
 import Image from "next/image";
+import WithAnimation from "./withAnimation";
 
 export default function ProfilePic() {
     const isMobile = useIsMobile();
@@ -9,17 +10,19 @@ export default function ProfilePic() {
     const size = isMobile ? (width * 75) / 100 : (height * 40) / 100;
     return (
         !!size && (
-            <div className="profile-img-border">
-                <Image
-                    src="/static/images/profile.webp"
-                    alt="Lorenzo Matias Lopez"
-                    className="profile-img"
-                    priority={true}
-                    draggable={false}
-                    width={size}
-                    height={size}
-                />
-            </div>
+            <WithAnimation>
+                <div className="profile-img-border">
+                    <Image
+                        src="/static/images/profile.webp"
+                        alt="Lorenzo Matias Lopez"
+                        className="profile-img"
+                        priority={true}
+                        draggable={false}
+                        width={size}
+                        height={size}
+                    />
+                </div>
+            </WithAnimation>
         )
     );
 }
--- src/components/withAnimation.js
+++ src/components/withAnimation.js
@@ -5,25 +5,21 @@ import { useEffect, useRef, useState } from "react";
 export default function WithAnimation({
     children,
     animationName = "opacity",
-    style,
     className,
 }) {
     const [isIntersecting, setIsIntersecting] = useState(false);
     const ref = useRef();
     useEffect(() => {
-        if (animationName && !isIntersecting) {
-            const observer = new IntersectionObserver(([entry]) => {
-                if (entry.isIntersecting) {
-                    setIsIntersecting(true);
-                } else {
-                    setIsIntersecting(false);
-                }
-            });
-            observer.observe(ref.current);
-            return () => observer.disconnect();
-        }
+        const observer = new IntersectionObserver(([entry]) => {
+            if (entry.isIntersecting) {
+                setIsIntersecting(true);
+            } else {
+                setIsIntersecting(false);
+            }
+        });
+        observer.observe(ref.current);
+        return () => observer.disconnect();
     }, [animationName, isIntersecting]);
-
     return animationName ? (
         <div
             ref={ref}
@@ -31,11 +27,10 @@ export default function WithAnimation({
             style={
                 isIntersecting
                     ? {
-                          ...style,
                           animationName,
                           animationDuration: "2s",
                       }
-                    : { ...style, opacity: 0 }
+                    : { opacity: 0 }
             }
         >
             {children}
