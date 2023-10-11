d76f925976259a545f6fd682bb6605797c11ad4d
lore64210 <lore64210@gmail.com>
2023-10-10 22:55:09 -0300

    fallback redirects in case no language is provided

--- next.config.js
+++ next.config.js
@@ -1,4 +1,48 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/es",
+                permanent: true,
+            },
+            {
+                source: "/about-me",
+                destination: "/es/about-me",
+                permanent: true,
+            },
+            {
+                source: "/terrain",
+                destination: "/es/terrain",
+                permanent: true,
+            },
+            {
+                source: "/maze",
+                destination: "/es/maze",
+                permanent: true,
+            },
+            {
+                source: "/balls-ecosystem",
+                destination: "/es/balls-ecosystem",
+                permanent: true,
+            },
+            {
+                source: "/path-finder",
+                destination: "/es/path-finder",
+                permanent: true,
+            },
+            {
+                source: "/traveling-salesman",
+                destination: "/es/traveling-salesman",
+                permanent: true,
+            },
+            {
+                source: "/linear-regression",
+                destination: "/es/linear-regression",
+                permanent: true,
+            },
+        ];
+    },
+};
